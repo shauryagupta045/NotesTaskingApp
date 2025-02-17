9905c10becd9f044510ab077e1ae0a64edeb1229
Shaurya Gupta <guptashaurya0728@gmail.com>
2025-02-17 23:43:21 +0530

    updated7

--- Frontend/src/components/Notes.jsx
+++ Frontend/src/components/Notes.jsx
@@ -88,7 +88,7 @@ const Notes = () => {
   const fetchNotes = async () => {
     try {
       const token = localStorage.getItem("token");
-      const response = await fetch("http://localhost:8080/api/notes", {
+      const response = await fetch(`https://notestaskingapp.onrender.com/api/notes`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
@@ -106,7 +106,7 @@ const Notes = () => {
     if (input.trim() !== "") {
       try {
         const token = localStorage.getItem("token");
-        const response = await fetch("http://localhost:8080/api/notes", {
+        const response = await fetch(`https://notestaskingapp.onrender.com/api/notes`, {
           method: "POST",
           headers: {
             Authorization: `Bearer ${token}`,
